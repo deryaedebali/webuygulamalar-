2760e9ee4bea69234342e62d1aac0ee53ab84538
deryaedebali <edebaliderya@gmail.com>
2025-11-22 18:24:38 +0000

    node.js dosyası olusturuldu

---
+++ index.js
@@ -0,0 +1,7 @@
+function addToCart(products){
+console.log(products)
+}
+
+let products=[{productName:"Telefon",quantity:2,unitPrice:5000},{productName:"PC",quantity:1,unitPrice:15000},{productName:"Bardak",quantity:3,unitPrice:20}]
+
+addToCart(products)
